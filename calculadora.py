7e4f006e40aea8bcb953ba3d09fc1439b9bbfb1d
Steven Torres Otrera <s_torres_otrera@hotmail.com>
2023-03-01 22:59:41 -0500

    Mi aporte Gherson Torres

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,10 @@ def suma(numero1, numero2):
     total= numero1 + numero2
     return total
 
+def resta(numero1, numero2):
+    total= numero1 - numero2
+    return total
+
 reultado = suma(num1, num2)
-print ('El resultado final es: ',reultado)
+reultado2 =resta(num1, num2)
+print ('El resultado final es: ',reultado2)
